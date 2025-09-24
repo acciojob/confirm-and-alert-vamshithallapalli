f13938cd6b91b4cb2dd803d1bf57d7387743798a
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-09-24 12:17:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function showConfirmation(){
+
+	var response = confirm('Do you want to proceed?');
+
+	if(response){
+		alert('You clicked OK. Proceeding...');
+	}
+	else{
+		alert('You clicked Cancel. Existing...');
+	}
+}
+
+window.onload = showConfirmation;
